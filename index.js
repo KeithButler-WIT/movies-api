a798ca314b426280056f2bf2d0fa32cb9d33e1cf
Keith Butler <keithbutler2001@gmail.com>
2023-08-06 16:01:47 +0100

    user router

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
+
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
+
 import './db';
 import './seedData'
 
@@ -15,6 +18,7 @@ app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
