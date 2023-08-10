c6903777c198570769c54e0805da6d39b1308ffe
Keith Butler <keithbutler2001@gmail.com>
2023-08-10 16:43:39 +0100

    Update Movies Data/Route

--- seedData/index.js
+++ seedData/index.js
@@ -1,7 +1,9 @@
 import userModel from '../api/users/userModel';
-import genreModel from '../api/genres/genreModel';
 import users from './users';
+import genreModel from '../api/genres/genreModel';
 import genres from './genres';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,6 +17,19 @@ async function loadUsers() {
     console.info(`${users.length} users were successfully stored.`);
   } catch (err) {
     console.error(`failed to Load user Data: ${err}`);
+  }
+}
+
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
   }
 }
 
@@ -33,4 +48,5 @@ async function loadGenres() {
 if (process.env.SEED_DB) {
   loadUsers();
   loadGenres();
+  loadMovies();
 }
